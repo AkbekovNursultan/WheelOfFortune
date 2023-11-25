ae4560b98b9cdc6da3b3687883dc8b4a39fea8a1
AkbekovNursultan <nursultan.akbekov@alatoo.edu.kg>
2023-11-25 22:31:08 +0600

    add showScoreBoard, playersScore

--- src/Main.java
+++ src/Main.java
@@ -14,10 +14,12 @@ public class Main {
                             "The most popular song of artist Stromae", "Lion, zebra, hippo and giraffe"};
         ArrayList<String>  playersList = new ArrayList<>();
         playersList = addPlayers(playersList, sc);
-        ArrayList<Integer> playersScore = new ArrayList<>();
+        int[] playersScore = new int[playersList.size()];
+        Arrays.fill(playersScore, 0);
         int randomChoice = ran.nextInt(0, words.length);
         String chosenWord = words[randomChoice];
         String chosenWordHint = wordsHints[randomChoice];
+        cleanScreen();
         System.out.println(chosenWord);
         //
         char[] correctLetters = chosenWord.toCharArray();
@@ -29,17 +31,19 @@ public class Main {
                 playerTurn = 0;
             }
             System.out.println("          Your turn " + playersList.get(playerTurn) + "!\n");
+            showScoreboard(playersList, playersScore);
             drawGameBoard(correctLetters, hiddenLetters, chosenWordHint);
             System.out.println("          Make your guess: ");
             String guessWord = sc.next();
             cleanScreen();
             if (guessWord.length() < 2) {
                 char guess = guessWord.charAt(0);
-                playerTurn = checkGuess(correctLetters, hiddenLetters, guess, playerTurn);
+                playerTurn = checkGuess(playersList, playersScore, correctLetters, hiddenLetters, guess, playerTurn);
             } else {
                 if (guessWord.toUpperCase().equals(chosenWord)) {
                     System.out.println("Congratulations, " + playersList.get(playerTurn) + "! You have guessed the word!");
-                    showTheAnswer(correctLetters);
+                    showTheAnswer(correctLetters, playersList, playersScore);
+                    System.out.println("\n\n     The WINNER is " + playersList.get(playerTurn).toUpperCase() + "!!!!!");
 
                     System.exit(0);
                 } else {
@@ -68,7 +72,13 @@ public class Main {
         }
         return newOrder;
     }
-    //static void showScoreboard(ArrayList<String> playersList, ArrayList<Integer> playersScore){}
+    static void showScoreboard(ArrayList<String> playersList, int[] playersScore){
+        for(int i = 0; i < playersList.size(); i++){
+            System.out.print("   " + playersList.get(i) + " -- " + playersScore[i] + " points");
+            System.out.println();
+        }
+        System.out.println();
+    }
 
     static void drawGameBoard(char[] correctLetters, char[] hiddenLetters, String chosenWordHint){
         for (char hiddenLetter : hiddenLetters) {
@@ -82,32 +92,39 @@ public class Main {
         System.out.println("Hint: " + chosenWordHint);
         System.out.println();
     }
-    static int checkGuess(char[] correctLetters,char[] hiddenLetters, char guess, int playerTurn) {
+    static int checkGuess(ArrayList<String> playersList, int[] playersScore, char[] correctLetters,char[] hiddenLetters, char guess, int playerTurn) {
         int lettersFound = 0;
         for (int i = 0; i < correctLetters.length; i++) {
             if (Character.toUpperCase(guess) == correctLetters[i] && hiddenLetters[i] != Character.toUpperCase(guess)) {
                 hiddenLetters[i] = correctLetters[i];
                 lettersFound++;
             } else if (Character.toUpperCase(guess) == hiddenLetters[i]) {
-                System.out.println("This letter was already guessed!\n");
+                System.out.println("      This letter was already guessed!\n");
                 lettersFound = -1;
                 break;
             }
         }
-        if(Arrays.equals(correctLetters, hiddenLetters)){
-            showTheAnswer(correctLetters);
-            System.exit(0);
-        }else if(lettersFound == 0){
-            System.out.println("There is no '"+ Character.toUpperCase(guess)+ "' in this word.\n");
+        if(lettersFound == 0){
+            System.out.println("      There is no '"+ Character.toUpperCase(guess)+ "' in this word.\n");
             playerTurn++;
         } else if(lettersFound == 1) {
-            System.out.println("There is 1 '" + Character.toUpperCase(guess) + "' letter!\n+100 points");
+            playersScore[playerTurn] += 100;
+            System.out.println("      There is 1 '" + Character.toUpperCase(guess) + "' letter!\n          +100 points");
         } else if(lettersFound > 1){
-            System.out.println("There are " + lettersFound + " '" + Character.toUpperCase(guess) + "' letters in this word!\n+" + (lettersFound * 100) + " points");
+            playersScore[playerTurn] += 100 * lettersFound;
+            System.out.println("  There are " + lettersFound + " '" + Character.toUpperCase(guess) + "' letters in this word!\n          +" + (lettersFound * 100) + " points");
+        }
+        if(Arrays.equals(correctLetters, hiddenLetters)){
+            cleanScreen();
+            cleanScreen();
+            showTheAnswer(correctLetters, playersList, playersScore);
+            System.exit(0);
         }
         return playerTurn;
     }
-    static void showTheAnswer(char[] correctLetters){
+    static void showTheAnswer(char[] correctLetters, ArrayList<String> playersList, int[] playersScore){
+        showScoreboard(playersList,playersScore);
+        System.out.println();
         for (char correctLetter : correctLetters) {
             System.out.print("  " + correctLetter + "     ");
         }
@@ -116,10 +133,6 @@ public class Main {
             System.out.print("=====   ");
         }
     }
-    static void finishGame(){
-
-    }
-
     static void cleanScreen(){
         System.out.print("\033[H\033[2J");
         System.out.flush();
